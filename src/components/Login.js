0ba906efc9cf330247d3f250d366a94ee917cc3c
rokkrav <mailtoravone@gmail.com>
2023-11-23 00:10:23 +0530

    after login validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,32 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidate } from "../utils/validate";
 
 const Login = () => {
   const [signIn, setSignIn] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
   const signToggle = () => {
     setSignIn(!signIn);
+  };
+
+  const handleSignInBtn = () => {
+    const message = checkValidate(email.current.value, password.current.value);
+    setErrorMessage(message);
   };
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-cover bg-black md:bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/a09bb938-2d90-42ae-986e-5a3e4abf9e77/8eb1e781-3494-4aa4-9405-268ca6473e4c/IN-en-20231113-popsignuptwoweeks-perspective_alpha_website_large.jpg')]">
       <Header />
-      {/* <div className="hidden lg:block bg-cover  bg-[url('https://assets.nflxext.com/ffe/siteui/vlv3/a09bb938-2d90-42ae-986e-5a3e4abf9e77/8eb1e781-3494-4aa4-9405-268ca6473e4c/IN-en-20231113-popsignuptwoweeks-perspective_alpha_website_large.jpg')]">
-        <img src="" alt="bg-img" />
-      </div> */}
-      <form className=" bg-black w-9/12 md:w-4/12  text-white p-12 bg-opacity-80">
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className=" bg-black w-10/12 md:w-4/12  text-white p-12 bg-opacity-80 rounded-md"
+      >
         <h1 className="font-bold text-3xl my-2 py-4">
           {signIn ? "Sign In" : "Sign Up"}
         </h1>
@@ -24,18 +38,24 @@ const Login = () => {
           />
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email Address"
           className="p-2 my-2 w-full rounded-sm  bg-neutral-700"
         />
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-2 my-2 w-full rounded-sm bg-neutral-700"
         />
-        <button className="py-4 mt-8 mb-2 rounded-sm font-semibold bg-red-700 w-full">
+        <button
+          className="py-4 mt-8 mb-2 rounded-sm font-semibold bg-red-700 w-full"
+          onClick={handleSignInBtn}
+        >
           {signIn ? "Sign In" : "Sign Up"}
         </button>
+        <p className="text-red-600 text-sm">{errorMessage}</p>
         <p
           className="font-normal mt-2 cursor-pointer text-slate-400"
           onClick={signToggle}
